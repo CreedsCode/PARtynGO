93758b19b00c3c22bb86f8164f303b9dc4b08555
owen <owen@owen>
2023-11-08 20:14:39 +0000

    design

--- frontend/src/pages/CreateGame.jsx
+++ frontend/src/pages/CreateGame.jsx
@@ -27,7 +27,7 @@ export default function CreateGame() {
       />
       <h1 className="mt-3">CREATE GAME</h1>
 
-      <div className="max-w-4xl w-full mt-10 rounded-2xl bg-white/10 backdrop-blur-2xl p-10">
+      <div className="max-w-4xl w-full mt-10 rounded-2xl bg-white/10 backdrop-blur-2xl p-6">
         <h2>Give your game a name to begin!</h2>
         <br />
 

68664afdd369ec5ae81cbd402b796b81643db219
owen <owen@owen>
2023-11-08 22:28:24 +0000

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,14 @@ function Home() {
     <>
       <Header />
 
-      <h1></h1>
+      <h1>welcome</h1>
+
+      <br />
+      <br />
+
+      <a href="/createGame">
+        <h2>start new game</h2>
+      </a>
     </>
   );
 }

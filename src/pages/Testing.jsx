2fec5e547f9d2949a6f29176917235d6087a511f
Dercio <contact@creedscode.cc>
2023-11-08 15:59:07 +0100

    Add tailwindcss

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Testing() {
   return (
-    <div>Testing</div>
-  )
+    <div>
+      Testing #<h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }

3c1d271b9ad10df2fe84cfb5786520ec96173e5c
Dercio <contact@creedscode.cc>
2023-11-08 17:56:31 +0100

    remove stuff

--- frontend/src/pages/Testing.jsx
+++ frontend/src/pages/Testing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
- async function Testing() {
+
+export default function Testing() {
   return (
     <div>
       Testing <h1 className="text-3xl font-bold underline">Hello world!</h1>

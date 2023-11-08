f356379eee965026a7ddccc989490576229a674f
owen <owen@owen>
2023-11-08 19:57:54 +0000

    first pass at design

--- frontend/src/pages/CreateGame.jsx
+++ frontend/src/pages/CreateGame.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import "./Background.css";
 
 export default function CreateGame() {
+  const [gameName, setGameName] = useState("big night out");
+
+  const handleChange = (event) => {
+    setGameName(event.target.value);
+  };
+
   return (
-    <div>CreateGame</div>
-  )
+    <div className="flex flex-col items-center">
+      <div class="bm-pl">
+        <div className="bm-pl__blob bm-pl__blob--r"></div>
+        <div className="bm-pl__blob bm-pl__blob--g"></div>
+        <div className="bm-pl__blob bm-pl__blob--b"></div>
+      </div>
+      <div class="bm-overlay"></div>
+
+      <img
+        src="/logo-pink.png"
+        alt="logo"
+        className="logo"
+        width="200"
+        height="200"
+      />
+      <h1 className="mt-3">CREATE GAME</h1>
+
+      <div className="max-w-4xl w-full mt-10 rounded-2xl bg-white/10 backdrop-blur-2xl p-10">
+        <div className="flex flex-row">
+          <input
+            type="text"
+            placeholder="Name"
+            value={gameName}
+            onChange={handleChange}
+            className="bg-white/30 font-bold rounded-xl p-2 w-full mr-5"
+          />
+          <button>start</button>
+        </div>
+      </div>
+    </div>
+  );
 }

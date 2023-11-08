a9da9cce498b74cb78ef634b889d748f20296d18
owen <owen@owen>
2023-11-08 20:10:03 +0000

    design

--- frontend/src/pages/CreateGame.jsx
+++ frontend/src/pages/CreateGame.jsx
@@ -28,15 +28,18 @@ export default function CreateGame() {
       <h1 className="mt-3">CREATE GAME</h1>
 
       <div className="max-w-4xl w-full mt-10 rounded-2xl bg-white/10 backdrop-blur-2xl p-10">
+        <h2>Give your game a name to begin!</h2>
+        <br />
+
         <div className="flex flex-row">
           <input
             type="text"
             placeholder="Name"
             value={gameName}
             onChange={handleChange}
-            className="bg-white/30 font-bold rounded-xl p-2 w-full mr-5"
+            className="bg-white/30 font-bold rounded-xl p-2 w-full mr-5 px-3"
           />
-          <button>start</button>
+          <button>create</button>
         </div>
       </div>
     </div>

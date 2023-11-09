da7b71c8a8c00593456b7a135c9166897626dc02
owen <owen@owen>
2023-11-09 11:14:25 +0000

    moved routes

--- frontend/src/app/game/[id]/lobby/Lobby.jsx
+++ frontend/src/app/game/[id]/lobby/Lobby.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
 
-import Header from "../components/Header";
+import Header from "@/components/Header";
 
 export default function Lobby() {
   const location = useLocation();
@@ -33,9 +32,10 @@ export default function Lobby() {
               ))}
             </ul>
             <a href="/game/:gameID">
-            <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 ease-in-out">
-    Start Game
-  </button></a>
+              <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 ease-in-out">
+                Start Game
+              </button>
+            </a>
           </div>
         </div>
       </div>

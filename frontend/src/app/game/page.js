f2470a2e2f5034a24abbd8f304233c26e988f427
owen <owen@owen>
2023-11-09 12:50:42 +0000

    changes

--- frontend/src/app/game/page.js
+++ frontend/src/app/game/page.js
@@ -1,7 +1,42 @@
 "use client";
 import React, { useState } from "react";
-
 import Header from "@/components/Header";
+
+import Link from "next/link";
+
+function CreateGameForm({ handleChange, gameName, generateLobbyCode }) {
+  return (
+    <>
+      <h2>Give your game a name to begin!</h2>
+      <br />
+
+      <div className="flex flex-row">
+        <input
+          type="text"
+          placeholder="Name"
+          value={gameName}
+          onChange={handleChange}
+          className="bg-white/30 font-bold rounded-xl p-2 w-full mr-3 px-3"
+        />
+        <button onClick={generateLobbyCode} className="text-white">
+          Create
+        </button>
+      </div>
+    </>
+  );
+}
+
+function GameCreated({ lobbyCode, gameName }) {
+  return (
+    <>
+      <h2>Game created!</h2>
+      <br />
+      <Link href={`/game/${lobbyCode}/lobby?name=${gameName}`}>
+        Join the lobby!
+      </Link>
+    </>
+  );
+}
 
 export default function CreateGame() {
   const [gameName, setGameName] = useState("big night out");
@@ -12,6 +47,7 @@ export default function CreateGame() {
   };
 
   const generateLobbyCode = () => {
+    console.log("Creating game...");
     const code = Math.random().toString(36).substr(2, 6).toUpperCase();
     setLobbyCode(code);
   };
@@ -23,30 +59,14 @@ export default function CreateGame() {
         <h1 className="mt-3">CREATE GAME</h1>
 
         <div className="w-full mt-10 rounded-2xl bg-white/10 backdrop-blur-2xl p-4">
-          <h2>Give your game a name to begin!</h2>
-          <br />
-
-          <div className="flex flex-row">
-            <input
-              type="text"
-              placeholder="Name"
-              value={gameName}
-              onChange={handleChange}
-              className="bg-white/30 font-bold rounded-xl p-2 w-full mr-3 px-3"
+          {lobbyCode ? (
+            <CreateGameForm
+              handleChange={handleChange}
+              gameName={gameName}
+              generateLobbyCode={generateLobbyCode}
             />
-            <button onClick={generateLobbyCode} className="text-white">
-              Create
-            </button>
-          </div>
-
-          {lobbyCode && (
-            <div>
-              <p>Lobby Code: {lobbyCode}</p>
-              {/* Pass the lobbyCode as a query parameter to the Lobby component */}
-              <Link to={`/lobby?code=${lobbyCode}&name=${gameName}`}>
-                Go to Lobby
-              </Link>
-            </div>
+          ) : (
+            <GameCreated lobbyCode={lobbyCode} gameName={gameName} />
           )}
         </div>
       </div>

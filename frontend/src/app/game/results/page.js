f3a3a5ab4b15e966b3c3b319c651c6861c75d4c6
owen <owen@owen>
2023-11-09 11:03:05 +0000

    icon and changes

---
+++ frontend/src/app/game/results/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "@/components/Header";
+
+export default function GameResults() {
+  return (
+    <>
+      <Header />
+
+      <h1 className="text-3xl">The results are in!</h1>
+
+      <br />
+      <p>results from openai</p>
+    </>
+  );
+}
